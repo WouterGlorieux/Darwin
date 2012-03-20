cfabcb50a7467a3a6910a6df9c84cca9b17e1fa1
Skidzobolder <skidzobolder@hotmail.com>
2012-03-20 10:43:14 +0100

    Tokamak first commit

---
+++ Tokamak/src/Tokamak.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Tokamak.cpp
+// Author      : Skidzobolder
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "rapidxml.hpp"
+
+
+int main() {
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
